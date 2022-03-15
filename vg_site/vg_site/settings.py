2432cbda8da429e7dc3345dfabb7e3c8bb83b251
Daniele Irto <danieleirto6@gmail.com>
2022-03-15 15:12:35 +0100

    Remove django secret

--- vg_site/vg_site/settings.py
+++ vg_site/vg_site/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qow*q46ewyf$b7=@=rchai^4p5#)k_=fd1e+xc=b#4we@*5gy6'
+f = open("secret.txt", "r")
+secret_key = f.read()
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
