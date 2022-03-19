117cdee62d4bd7e10a93cc0606b214951fe3979a
Irene Ferfoglia <irene.ferfo@gmail.com>
2022-03-19 10:35:34 +0100

    new file

---
+++ get_colors.py
@@ -0,0 +1,40 @@
+from collections import Counter
+from sklearn.cluster import KMeans
+from matplotlib import colors
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+def preprocess(raw):
+    image = cv2.resize(raw, (900, 600), interpolation = cv2.INTER_AREA)                                          
+    image = image.reshape(image.shape[0]*image.shape[1], 3)
+    return image
+
+    
+def rgb_to_hex(rgb_color):
+    hex_color = "#"
+    for i in rgb_color:
+        hex_color += ("{:02x}".format(int(i)))
+    return hex_color
+
+
+def analyze(img):
+    clf = KMeans(n_clusters = 10, random_state = 0)
+    color_labels = clf.fit_predict(img)
+    center_colors = clf.cluster_centers_ # in RGB
+
+    counts =  Counter(color_labels)
+    ordered_colors = [rgb_to_hex(center_colors[i]) for i in counts.keys()]
+
+    d = {}
+    for i in range(len(ordered_colors)):
+        d[ordered_colors[i]] = list(counts.values())[i]
+    d = {k: v for k, v in sorted(d.items(), key=lambda item: item[1], reverse = True)}
+
+    plt.figure(figsize = (12, 8))
+    plt.pie(d.values(), labels = d.keys(), colors = d.keys())
+
+
+def extract_colors(image):
+    modified_image = preprocess(image)
+    analyze(modified_image)
