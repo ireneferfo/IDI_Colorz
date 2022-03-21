7d2f08735039b6d9d4648da9fa12ea4a678b2f54
Isacco <isacco.zinna@gmail.com>
2022-03-21 17:13:06 +0100

    Author pages.

--- vg_site/vg_app/urls.py
+++ vg_site/vg_app/urls.py
@@ -4,13 +4,13 @@ from rest_framework.routers import DefaultRouter
 
 app_name = 'vg_app'
 
-router = DefaultRouter()
-router.register(r'pictures', views.PictureView)
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('picture/', views.PictureListView
          .as_view(), name='picture'),
     path('picture/<int:pk>', views.PictureDetailView.as_view(), name='picture-detail'),
-    path('picture/random/', views.random_picture_detail, name='random')
+    path('picture/random/', views.random_picture_detail, name='random'),
+    path('artist/', views.ArtistListView
+         .as_view(), name='artist'),
+    path('artist/<int:pk>', views.ArtistDetailView.as_view(), name='artist-detail')
 ]
--- vg_site/vg_app/views.py
+++ vg_site/vg_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import HttpResponse
 from django.views import generic
 from rest_framework import viewsets
 from vg_app.serializers import PictureSerializer
-from vg_app.models import Picture
+from vg_app.models import Picture, Artist
 from url_filter.filtersets import ModelFilterSet
 from django.http import JsonResponse
 from .image_parser import parse
@@ -25,28 +25,14 @@ class PictureListView(generic.ListView):
 class PictureDetailView(generic.DetailView):
     model = Picture
 
+class ArtistListView(generic.ListView):
+    model = Artist
 
-class PictureView(viewsets.ReadOnlyModelViewSet):
-    queryset = Picture.objects.all()
-    serializer_class = PictureSerializer
-
-
-def picture_list(request):
-    picture = Picture.objects.all()
-    serializer = PictureSerializer(picture, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    paginate_by = 50
 
 
-def picture_detail(request, pk):
-    try:
-        picture = Picture.objects.get(pk=pk)
-    except Picture.DoesNotExist:
-        return HttpResponse(status=404)
-    serializer = PictureSerializer(picture)
-    image = parse(serializer)
-    with open(image, 'rb') as i:
-        return HttpResponse(i.read(), content_type='image/jpeg')
-
+class ArtistDetailView(generic.DetailView):
+    model = Artist
 
 def random_picture_detail(request):
     picture = Picture.objects.all()
