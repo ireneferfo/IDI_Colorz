f23bcc23fddf7f4c200f355dec9e9b69d7fb2704
Daniele Irto <danieleirto6@gmail.com>
2022-03-22 16:07:52 +0100

    First try search

--- vg_site/vg_app/urls.py
+++ vg_site/vg_app/urls.py
@@ -6,11 +6,10 @@ app_name = 'vg_app'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('picture/', views.PictureListView
-         .as_view(), name='picture'),
+    path('picture/', views.PictureListView.as_view(), name='picture'),
     path('picture/<int:pk>', views.PictureDetailView.as_view(), name='picture-detail'),
     path('picture/random/', views.random_picture_detail, name='random'),
-    path('artist/', views.ArtistListView
-         .as_view(), name='artist'),
-    path('artist/<int:pk>', views.ArtistDetailView.as_view(), name='artist-detail')
+    path('artist/', views.ArtistListView.as_view(), name='artist'),
+    path('artist/<int:pk>', views.ArtistDetailView.as_view(), name='artist-detail'),
+    path('picture/search', views.PictureSearchView.as_view(), name='search')
 ]
--- vg_site/vg_app/views.py
+++ vg_site/vg_app/views.py
@@ -7,7 +7,7 @@ from url_filter.filtersets import ModelFilterSet
 from django.http import JsonResponse
 from .image_parser import parse
 from random import randint
-from django.db.models import Max
+from django.db.models import Max, Q
 from django.shortcuts import render, redirect
 
 
@@ -16,26 +16,33 @@ from django.shortcuts import render, redirect
 def index(request):
     return render(request, 'index.html')
 
+def random_picture_detail(request):
+    picture = Picture.objects.all()
+    max = picture.aggregate(Max('id'))
+    pk = randint(1, max['id__max'])
+    return redirect('vg_app:picture-detail', pk=pk)
 
 class PictureListView(generic.ListView):
     model = Picture
     paginate_by = 50
-
 
 class PictureDetailView(generic.DetailView):
     model = Picture
 
 class ArtistListView(generic.ListView):
     model = Artist
-
     paginate_by = 50
-
 
 class ArtistDetailView(generic.DetailView):
     model = Artist
 
-def random_picture_detail(request):
-    picture = Picture.objects.all()
-    max = picture.aggregate(Max('id'))
-    pk = randint(1, max['id__max'])
-    return redirect('vg_app:picture-detail', pk=pk)
+class PictureSearchView(generic.ListView):
+    model = Picture
+    def get_queryset(self):
+        title = self.request.GET.get('title')
+        year = self.request.GET.get('year')
+        #artist = self.request.GET.get('artist')
+        object_list = Picture.objects.filter(
+        Q(Title__icontains=title) or Q(Year__icontains=year)
+        )
+        return object_list
