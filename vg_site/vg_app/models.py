0bfc820c585b145df209b7d252852b9b4e492803
Daniele Irto <danieleirto6@gmail.com>
2022-03-28 18:02:35 +0200

    Added ordering, year field, first/last page button

--- vg_site/vg_app/models.py
+++ vg_site/vg_app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.utils import timezone
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db.models import F
 
 class Artist(models.Model):
+    class Meta:
+        ordering = ['Name']
     Artist_ID = models.IntegerField(default=0, unique=True)
     Name = models.CharField(max_length=50)
     Artist_url = models.CharField(max_length=100)
@@ -16,10 +19,11 @@ class Color(models.Model):
     Quantity = models.FloatField(default=0)
 
 class Picture(models.Model):
+    class Meta:
+        ordering = ['Artist__Name', F('Year').asc(nulls_last=True), 'Title']
     Picture_ID = models.IntegerField(default=0, unique=True)
     Title = models.CharField(max_length=50)
-    Year = models.PositiveSmallIntegerField(validators=[MinValueValidator(1000),
-    MaxValueValidator(timezone.now().year)], null=True)
+    Year = models.PositiveSmallIntegerField(validators=[MinValueValidator(1000), MaxValueValidator(timezone.now().year)], null=True)
     Artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
     Width = models.PositiveSmallIntegerField()
     Height = models.PositiveSmallIntegerField()
