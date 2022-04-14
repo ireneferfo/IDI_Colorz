dd7ab451605c2ff8b35e6c87a521ab54f5b765d4
Isacco <isacco.zinna@gmail.com>
2022-04-14 16:01:10 +0200

    Adjusted settings

--- vg_site/settings.py
+++ vg_site/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Default primary key field type
