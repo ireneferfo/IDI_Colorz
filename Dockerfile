4eb52d294bfdf2e279ef7528f278cd2caee5f252
Isacco <isacco.zinna@gmail.com>
2022-04-14 12:28:11 +0200

    Setted for heroku.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 FROM python:3.9
 
+ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
+ENV DEBUG 0
 
 WORKDIR /app
 
 ADD . /app
 
 RUN pip install -r requirements.txt
+
+RUN python manage.py collectstatic --noinput
+
+CMD gunicorn vg_site.wsgi:application --bind 0.0.0.0:$PORT
--- vg_site/settings.py
+++ vg_site/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+	"whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
